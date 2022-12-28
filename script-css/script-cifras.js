7e705160909afab771f5bff43f1ad56360afc663
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2022-12-28 12:00:06 -0300

    Atualizando

--- script-css/script-cifras.js
+++ script-css/script-cifras.js
@@ -1,18 +1,12 @@
-var icon1=document.getElementById('icon-1')
-icon1.addEventListener('mouseover', over1)
-var abbr1 = document.querySelector('#res')
-var b = document.querySelector(".b")
 
-b.addEventListener('click', clicou)
-/*const el = document. querySelector("#container")
+
+
+
+/*var abbr1 = document.querySelector('#res')
+const el = document. querySelector("#content-right")
 el. addEventListener("click", (e) => {
 const rect = e. target. getBoundingClientRect();
 const x = e. clientX - rect. left;
 const y = e. clientY - rect. top;
 abbr1.innerHTML=("Esquerda: " + x + " - Topo: " + y);
 })*/
-
-
-function clicou(){
-    abbr1.innerHTML="ola"
-}
